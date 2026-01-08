138a44fa859d3a2a0480d4ef1d838354bb9c34cc
fbk <frkle@hvl.no>
2026-01-08 14:23:52 +0100

    looks alright, now the segmentation

--- refdata/metrics.py
+++ refdata/metrics.py
@@ -68,7 +68,7 @@ def get_roted_from_claude_because_im_stupid(this_df, in_degrees=False,is_mocap=F
     df_corrected[angle_cols] = corrected_angles
     
     if is_mocap: ## I cant fix this correctly with adequate math and i am growing desperate. sorry.
-        df_corrected[PA] *= -1
+        df_corrected[PB] *= -1
     return df_corrected
 
 
